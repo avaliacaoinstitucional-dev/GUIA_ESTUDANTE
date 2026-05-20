ad4d897d95bddba3251eea059a6fe715bcfc8dc5
Otavides Almeida <otavidesalmeida@users.noreply.github.com>
2026-05-20 15:36:46 -0400

    up

--- assets/js/components/courseCard.js
+++ assets/js/components/courseCard.js
@@ -17,6 +17,7 @@ const areaImages={
 };
 const courseImages={
   'tecnologia-em-agrocomputacao':'assets/img/courses/agrocomputacao.jpg',
+  'tecnologia-em-cafeicultura':'assets/img/courses/cafeicultura.jpg',
   'tecnologia-em-analise-e-desenvolvimento-de-sistemas':'assets/img/courses/analise-desenvolvimento-sistemas.jpg',
   'tecnologia-em-agroindustria':'https://images.unsplash.com/photo-1635174815483-81657b67ec48?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'tecnologia-em-banco-de-dados':'assets/img/courses/banco-de-dados.jpg',
